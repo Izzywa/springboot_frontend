c216611a9f1d2547160b56134fe7a0bfe57bad2a
Izzatul Najwa binti Ismail <inbi1@student.london.ac.uk>
2026-01-06 08:28:27 +0800

    test backend

--- src/modules/Dashboard/Dashboard.tsx
+++ src/modules/Dashboard/Dashboard.tsx
@@ -1,5 +1,19 @@
+import { useEffect, useState } from "react";
+
 export default function Dashboard() {
+   const [tasks, setTasks] = useState<any[]>([]);
+   useEffect(() => {
+      fetch(import.meta.env.VITE_TASKS_URL!)
+      .then(res => res.json())
+      .then(data => setTasks(data))
+      .catch(err => console.error("Error fetching tasks:", err));
+   }, [])
    return (<div>
     Dashboard Page
+      <ul>
+        {tasks.map(task => (
+          <li key={task.id}>{task.name}</li>
+        ))}
+      </ul>
    </div>)
 }
